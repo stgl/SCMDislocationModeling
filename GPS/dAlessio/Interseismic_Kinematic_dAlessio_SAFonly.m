181550b69b6df8c780f4b55d72e8a537d695f6cf
George Hilley <gehilley@gmail.com>
2019-12-17 11:39:09 -0800

    cleanup

---
+++ GPS/dAlessio/Interseismic_Kinematic_dAlessio_SAFonly.m
@@ -0,0 +1,97 @@
+addpath(genpath('/data/cees/hilley/SCM_Aron/Gmsh'))
+addpath(genpath('/data/cees/aron/bin/matlab'))
+
+slip_creep_vec=[0.0].*-1e-6;
+
+%gps_file='obs_data_gps.txt';
+
+%xyz_gps=load(gps_file);
+
+x_min = 521.95;
+x_max = 633.82;
+y_min = 4072.72;
+y_max = 4206.88;
+
+dx = 0.5;
+[X,Y] = meshgrid([x_min:dx:x_max],[y_min:dx:y_max]);
+sn = size(X);
+nx = sn(1);
+ny = sn(2);
+xyz = [reshape(X,prod(size(X)),1) reshape(Y,prod(size(Y)),1) zeros(prod(size(Y)),1)];
+
+obs_data=struct('x',xyz(:,1),'y',xyz(:,2),'z',xyz(:,3),'v',v);
+
+faultnames=['SAF_SF_Pen_SCM_SJB.msh', ' '];
+
+faults=ReadPatches(faultnames);
+
+ends = cumsum(faults.nEl); % Give ending indices of the n faults
+begs = [1; ends(1:end-1)+1]; % Give beginning indices of the n faults
+centroids=PatchCentroid(faults.c,faults.v); % xyz of faults centroids, km ENU
+bc = zeros(sum(faults.nEl), 3);
+
+d = zeros(sum(faults.nEl), 3);
+for i=1:length(faults.nEl)
+    d(begs(i):ends(i), :) = repmat([slip_creep_vec(i) 0 0], faults.nEl(i), 1); % Assign [strike-slip 0 0] as the magnitude of boundary conditions to faults
+end
+
+d = [17.1 0 0.2;
+     17.1 0 0.2;
+     16.8 0 3.3;
+     16.8 0 3.3;
+     16.4 0 4.9;
+     16.4 0 4.9;
+      9.0 0 0.0;
+      9.0 0 0.0;
+     16.4 0 4.9;
+     16.4 0 4.9;
+      9.0 0 0.0;
+      9.0 0 0.0;
+     16.9 0 2.5;
+     16.9 0 2.5;
+     16.9 0 2.5;
+     16.9 0 2.5;
+     16.9 0 2.5;
+     16.9 0 2.5;
+     16.9 0 2.5;
+     16.9 0 2.5;
+     16.9 0 2.5;
+     16.9 0 2.5;
+     16.9 0 2.5;
+     16.9 0 2.5;
+     16.9 0 2.5;
+     16.9 0 2.5;
+     16.9 0 2.5;
+     16.9 0 2.5;
+     16.9 0 2.5;
+     16.9 0 2.5;
+     16.9 0 2.5;
+     16.9 0 2.5;
+     20.2 0 -1.9;
+     20.2 0 -1.9;] * -1E-6;
+
+d(:,3) = 0.0;
+
+[slip_kine, trac_kine, obs_kine] = tribemx(faults, d, bc, obs_data);
+
+u = reshape(obs_kine.u,3,length(obs_kine.u)/3)';
+UX = reshape(u(:,1),nx,ny);
+UY = reshape(u(:,2),nx,ny);
+UZ = reshape(u(:,3),nx,ny);
+
+figure(1)
+meshview(faults, d(:,1));
+hold on
+maxval = max(abs(UZ(:)));
+imagesc(X(1,:),Y(:,1), UZ, [-maxval maxval]);
+colormap jet
+colorbar
+
+figure(2)
+quiver(X,Y,UX,UY);
+
+figure(3)
+imagesc(X(1,:),Y(:,1),UZ,[-maxval maxval]);
+colormap jet
+colorbar
+set(gca,'ydir','normal');axis equal;axis image
--- GPS/dAlessio/Interseismic_Kinematic_dAlessio_all_but_SAF.m
+++ GPS/dAlessio/Interseismic_Kinematic_dAlessio_all_but_SAF.m
@@ -2,7 +2,7 @@
 disp(' ')
 disp('%%% BEM Kinematic SCM, dAlessio faults, BAVU GPS Stations %%%')
 disp(' ')
-t_tot=0;    
+t_tot=0;
 % c=clock;                %comment before sending to CEES
 % PID=feature('getpid');  %comment before sending to CEES
 disp(['Date/time in Stanford= ',num2str(c(1)),' ',num2str(c(2)),' ',...
@@ -52,7 +52,7 @@ obs_data=struct('x',xyz(:,1),'y',xyz(:,2),'z',xyz(:,3),'v',v);
 % Faults long term interseismic slip
 SG_filename= 'SG_slip_lt.msh';
 RC_H_filename= 'RC_H_slip_lt.msh';
-WN_filename= 'VM_slip_lt.msh';
+WN_filename= 'WN_slip_lt.msh';
 C_north_filename= 'C_north_slip_lt.msh';
 C_cen_south_filename= 'C_cen_south_slip_lt.msh';
 GV_Con_filename= 'GV_Con_slip_lt.msh';
@@ -88,7 +88,7 @@ disp('Nr of free parameters= 0')
 disp(['Nr obs points= ',num2str(length(xyz)),' (gps= ',len_gps,', grid= ',len_grid,')'])
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%% Assigning kinematic BCs dAlessio model %%%% 
+%%% Assigning kinematic BCs dAlessio model %%%%
 
 d = zeros(sum(faults.nEl), 3); % Initialize the boundary condition array
 bc = d; % Initialize the boundary condition type array
@@ -104,7 +104,7 @@ disp('Run kinematic interseismic dAlessio model...')
 
 if G_out
     disp('Saving G ic-matrix to workspace= true')
-    [slip_kine, trac_kine, obs_kine, G] = tribemx(faults, d, bc, obs_data);   
+    [slip_kine, trac_kine, obs_kine, G] = tribemx(faults, d, bc, obs_data);
 else
     disp('Saving G ic-matrix to workspace= false')
     [slip_kine, trac_kine, obs_kine] = tribemx(faults, d, bc, obs_data);
@@ -116,7 +116,7 @@ end
 if save_output
     filename = [run_tag,'_output_SCM_kinematic_dAlessio_',num2str(c(1)),'_',num2str(c(2)),'_',...
     num2str(c(3))];
-    filename = strcat(filename,['_PID',num2str(PID),'.mat']);    
+    filename = strcat(filename,['_PID',num2str(PID),'.mat']);
     save(filename,'slip_kine','trac_kine','obs_kine')
     % save(['G_cl',cl,'_chi_grid.mat'],'G','-v7.3')
     disp('Saving outputs= true')
